5a80a30b0debf554a17f89a373193276f4382057
Sergey Rykov <sergeyrykov@Sergeys-MacBook-Pro.local>
2024-01-21 17:33:47 +0100

    minor frontend typos fix

--- streamlit_sql_app.py
+++ streamlit_sql_app.py
@@ -11,7 +11,7 @@ system_prompt="""You are an expert in salmon fish farming, salmon busines and
     header: str description: this is the header or the article
     tags: str description: this is tags to help you search, inlude companies, people, locations
     article: str description: actual article
-    Limit yourself with maximum 20 artciles except the case where you are asked to provide the list of articles 
+    Limit yourself with maximum 10 artciles except the case where you are asked to provide the list of articles 
     """
 
 service_context = ServiceContext.from_defaults(llm=llm, system_prompt=system_prompt )
@@ -44,7 +44,7 @@ query_engine = NLSQLTableQueryEngine(sql_database=sql_database, service_context=
 # Streamlit interface
 #st.title()
 st.markdown("<h2 style='text-align: justify; color: grey;'>Industry data about salmon fish farming and fish industry in general</h2>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: justify; color: grey;'>The knoledge base is from 2021</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: justify; color: grey;'>The knowledge base is from 2021</h3>", unsafe_allow_html=True)
 
 # Check for the session state key, initialize if not present
 if 'history' not in st.session_state:
